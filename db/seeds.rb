b3cd66c8a0a26eb90cfb75213e6cdbbca8f5d2e9
Andrew Owens <andrrowens@gmail.com>
2023-06-05 22:40:53 -0600

    created initial test seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Deleting old data..." 
+
+Andrew.destroy_all   
+Project.destroy_all
+
+puts "Creating seed data..." 
+
+a1 = Andrew.create(name: "Andrew Owens", background: "test background info about me!", description: "Newly graduated from Flatiron School's Software Engineering program. Looking for my next great opportunity.")
+
+p1  = Project.create(
+    title: "Rooted Plant Exchange",
+    phase: 5,
+    description: "Rooted Plant Exchange was created to allow gardeners, growers, and green thumbs to connect with the plant-loving community and to allow users to trade and give away plants to others in their area. It also provides educational information through the Plant Library and User Submitted Plants section.",
+    video: "https://www.loom.com/share/2972fb5fa9ef4c8c8817d8eda7437e3d",
+    github: "https://github.com/andrrowens/rooted-plant-exchange-frontend",
+    image1: "test",
+    image2: "test",
+    image3: "test")
+
+puts "✅ Done seeding!"
+
+
+# t.string "title"
+# t.integer "phase"
+# t.text "description"
+# t.string "video"
+# t.string "github"
+# t.string "image1"
+# t.string "image2"
+# t.string "image3"
